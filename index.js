b484a29de70c02d5aaf0d442cd591acd34c0081d
Dennis Kimathi <kimathidennis38@gmail.com>
2022-07-05 22:26:34 +0300

    build express server

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express(); //Use express Library
+const PORT = 8888; //Port number
+
+app.get("/status", (req, res) => {
+    const localTime = (new Date()).toLocaleDateString();
+
+    res
+    .status(200)
+    .send(`Server time is ${localTime}.`)
+
+})
+
+//Catch all route
+app.get("*", (req, res) => {
+    res.sendStatus(404); //Page not Found
+
+});
+
+//Initialize server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
